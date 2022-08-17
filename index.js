083b29aee5dc699efd326a032e1e8df176e5f413
Lauren Metz <Laurenmetz15@gmail.com>
2022-08-17 12:26:26 -0400

    add function to render album card and add to the page, album img not yet rendered. Changed size of enter artist header

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 document.addEventListener('DOMContentLoaded',function() {
     let artistForm = document.querySelector('form');
-    artistForm.addEventListener('submit', getArtistInfo)
+    artistForm.addEventListener('submit', renderAlbums)
 
 
 })
 
-function getArtistInfo(e) {
+function renderAlbums(e) {
     e.preventDefault();
     let artistInput = e.target.children[1].value;
-    let albums = []
+    let albums = [];
     fetch(`https://itunes.apple.com/search?media=music&entity=album&term=${artistInput}`).then(resp => resp.json())
     .then(data => {
         albums = data.results;
         console.log(albums);
+        albums.forEach(album => createAlbumCard(album));
     });
     return albums;
         
+}
+
+function createAlbumCard(album) {
+    let albumCard = document.createElement('card');
+    albumCard.classList.add('albums');
+    
+    let albumTitle = document.createElement('h2');
+    albumTitle.textContent = album.collectionName;
+    let albumArt = document.createElement('img');
+    //albumArt.src = album.artWorkUrl100;
+    let albumLink = document.createElement('a');
+    albumLink.href = album.collectionViewUrl;
+    let albumGenre = document.createElement('h3');
+    albumGenre.textContent = album.primaryGenreName;
+    let albumDate = document.createElement('p');
+    albumDate.classList.add('release-dates');
+    albumDate.textContent = album.releaseDate;
+
+    albumCard.append(albumTitle);
+    albumCard.append(albumArt);
+    albumCard.append(albumGenre);
+    albumCard.append(albumLink);
+    albumCard.append(albumDate);
+
+    document.querySelector('#timeline').append(albumCard);
+    console.log(albumCard);
+
 }
